c1b58bbdb43761e599431600ab80e8fe22ebb1a8
poulzinho <paul.gualotuna@tum.de>
2016-08-18 19:35:04 +0200

    Custom pokemon markers and info

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/// 1st Milestone - POKEMON MAP
 // require leaflet.js
 var L = require('leaflet');
 
@@ -6,7 +7,7 @@ L.Icon.Default.imagePath = 'node_modules/leaflet/dist/images/';
 
 // initialize the map using index.html #map id
 var map = L.map('map', {
-    scrollWheelZoom: true 
+    scrollWheelZoom: true
 });
 
 // set the position (latitude, longitude) and zoom level of the map
@@ -14,9 +15,11 @@ var map = L.map('map', {
 map.setView([48.262299, 11.669776], 16);
 
 // set an attribution string
-var attribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> ' +
+var attribution = ' JS16 <a href="https://github.com/PokemonGoers/PokeMap-1">PokeMap</a>, ' +
+    'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> ' +
     'contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-    'Imagery © <a href="http://thunderforest.com">Thunderforest/OpenCycleMap</a>';
+    'Imagery © <a href="http://thunderforest.com">Thunderforest/OpenCycleMap</a>, ' +
+    'Pokemon Images © <a href="http://pokemondb.net/">Pokémon Database</a>';
 
 // set the tiles the map will use (Thunderforest/OpenCycleMap)
 var tiles = 'http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png';
@@ -29,3 +32,48 @@ var layer = L.tileLayer(tiles, {
 
 // add the tile layer to the map
 layer.addTo(map);
+
+//// 2nd Milestone - POKEMON PAST LOCATION
+//// 3rd Milestone - POKEMON INFO
+
+// custom icons
+var LeafIcon = L.Icon.extend({
+    options: {
+        iconSize:     [50, 46],
+        popupAnchor:  [-7, -20]
+    }
+});
+
+// mark Pokemons
+L.marker([48.262299, 11.669776], {icon: new LeafIcon({iconUrl: 'map/css/images/pokemon/pikachu.gif'})})
+    .addTo(map).bindPopup('Pokemon: <b>Pikachu</b><br/> <br/>' +
+    'Type: <b>Electric</b> <br/>' +
+    'Species: <b>Mouse Pokemon</b> <br/>' +
+    'Evolution: Pichu &rarr; (Hapiness) &rarr; Pikachu &rarr; (Thunderstone) &rarr;  Raichu<br/>');
+
+
+var LeafIcon = L.Icon.extend({
+    options: {
+        iconSize:     [47, 42],
+        popupAnchor:  [-7, -20]
+    }
+});
+
+L.marker([48.264507, 11.669311], {icon: new LeafIcon({iconUrl: 'map/css/images/pokemon/bulbasaur.gif'})})
+    .addTo(map).bindPopup('Pokemon: <b>Bulbasaur</b><br/> <br/>' +
+    'Type: <b>Grass, Poison</b> <br/>' +
+    'Species: <b>Seed Pokémon</b> <br/>' +
+    'Evolution: Bulbasaur &rarr; (Level 16) &rarr; Ivysaur &rarr; (Level 32) &rarr;  Venusaur<br/>');
+
+var LeafIcon = L.Icon.extend({
+    options: {
+        iconSize:     [75, 54],
+        popupAnchor:  [-7, -20]
+    }
+});
+
+L.marker([48.265027, 11.673105], {icon: new LeafIcon({iconUrl: 'map/css/images/pokemon/mewtwo.gif'})})
+    .addTo(map).bindPopup('Pokemon: <b>Mewtwo</b><br/> <br/>' +
+    'Type: <b>Psychic</b> <br/>' +
+    'Species: <b>Genetic Pokemon</b> <br/>' +
+    'Evolution: Mewtwo does not evolve');
--- bundles/bundle.js
+++ bundles/bundle.js
@@ -1,4 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/// 1st Milestone - POKEMON MAP
 // require leaflet.js
 var L = require('leaflet');
 
@@ -30,6 +31,12 @@ var layer = L.tileLayer(tiles, {
 
 // add the tile layer to the map
 layer.addTo(map);
+
+//// 2nd Milestone - POKEMON PAST LOCATION
+//// 3rd Milestone - POKEMON INFO
+
+layer.marker([48.262299, 11.669776]).addTo(map).bindPopup('Pokemon info...');
+
 },{"leaflet":2}],2:[function(require,module,exports){
 /*
  Leaflet, a JavaScript library for mobile-friendly interactive maps. http://leafletjs.com
