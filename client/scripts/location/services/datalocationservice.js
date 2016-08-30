a8aa5eb67570cb150d46ab73f91136cc261d2c37
poulzinho <paul.gualotuna@tum.de>
2016-08-30 21:56:24 +0200

    WIP: consuming data from https://jsonstub.com

--- client/scripts/location/services/datalocationservice.js
+++ client/scripts/location/services/datalocationservice.js
@@ -8,10 +8,19 @@ module.exports = function (app) {
     app.factory(fullname, ['$http', function service($http) {
 
         function getPokemonLocations() {
-            return $http.get('images/app/mockup/dataSource-location.json')
-                .then(function (response) {
-                    return response.data;
-                });
+            return $http({
+                url: 'http://jsonstub.com/locations',
+                method: 'GET',
+                dataType: 'json',
+                data: '',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'JsonStub-User-Key': 'e593d8da-8b04-4ed2-9976-aaf8e1343e5f',
+                    'JsonStub-Project-Key': '7a3005e5-72c7-4ea1-bd70-ccc0d737106c'
+                }
+            }).then(function (response) {
+                return response.data;
+            });
         }
 
         var service = {
--- client/scripts/pokemon/services/datapokemonservice.js
+++ client/scripts/pokemon/services/datapokemonservice.js
@@ -8,24 +8,42 @@ module.exports = function (app) {
     app.factory(fullname, ['$http', function service($http) {
 
         function getPokemonIcons() {
-            return $http.get('images/app/pokemon/icons.json')
-                .then(function (response) {
-                    return response.data[0];
-                });
+            return $http({
+                url: 'http://jsonstub.com/icons',
+                method: 'GET',
+                dataType: 'json',
+                data: '',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'JsonStub-User-Key': 'e593d8da-8b04-4ed2-9976-aaf8e1343e5f',
+                    'JsonStub-Project-Key': '7a3005e5-72c7-4ea1-bd70-ccc0d737106c'
+                }
+            }).then(function (response) {
+                return response.data[0];
+            });
         }
 
         function getPokemon(pokeName) {
-            return $http.get('images/app/mockup/pokemon/' + pokeName + '.json')
-                .then(function (response) {
-                    var pokemonData = response.data;
-                    return {
-                        info: '<b>' + pokemonData.name.toUpperCase() + '</b>'
-                        + '<br/>'
-                        + 'Height: ' + '<b>' + pokemonData.height + '</b>'
-                        + '<br/>'
-                        + 'Weight: ' + '<b>' + pokemonData.weight + '</b>'
-                    };
-                });
+            return $http({
+                url: 'http://jsonstub.com/pokemon/' + pokeName,
+                method: 'GET',
+                dataType: 'json',
+                data: '',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'JsonStub-User-Key': 'e593d8da-8b04-4ed2-9976-aaf8e1343e5f',
+                    'JsonStub-Project-Key': '7a3005e5-72c7-4ea1-bd70-ccc0d737106c'
+                }
+            }).then(function (response) {
+                var pokemonData = response.data;
+                return {
+                    info: '<b>' + pokemonData.name.toUpperCase() + '</b>'
+                    + '<br/>'
+                    + 'Height: ' + '<b>' + pokemonData.height + '</b>'
+                    + '<br/>'
+                    + 'Weight: ' + '<b>' + pokemonData.weight + '</b>'
+                };
+            });
         }
 
         var service = {
