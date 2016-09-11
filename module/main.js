267ae350591b4d9d160679b943c602ec6253dee3
Oleksandr Fedotov <ga47jiz@mytum.de>
2016-09-11 12:06:44 +0200

    merge pokemons mock data

--- module/main.js
+++ module/main.js
@@ -151,7 +151,6 @@
             options: {
                 iconSize:     [30, 30],
                 shadowSize:   [50, 64],
-                iconAnchor:   [22, 94],
                 shadowAnchor: [4, 62],
                 popupAnchor:  [-3, -76]
             }
