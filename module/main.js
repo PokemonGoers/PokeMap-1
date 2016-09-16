abb2c683e9179e94c86e0ccaaa7d893feae98415
poulzinho <paul.gualotuna@tum.de>
2016-09-16 19:44:42 +0200

    Pokemap constructor according to interface from PokemonGoers/Catch-em-all#24, goto method to goTo with one Object as parameter, renamed lat / lon to latitud/longitud

--- module/main.js
+++ module/main.js
@@ -1,41 +1,61 @@
 'use strict';
 
-// coordinates - { lat: Number, lng: Number }
-// zoomLevel - Number
-// timeRange - { start: Number, end: Number }
+// options - {
+//     coordinates: {       // optional
+//         latitud: 48.1351,    // optional
+//         longitud: 11.5820     // optional
+//     },
+//     zoomLevel: 10,       // optional
+//     timeRange: 1,        // optional
+//     apiEndpoint: 'URI'   // mandatory
+// }
 
 (function () {
 
-    function PokeMap(htmlElement, coordinates, zoomLevel, timeRange, tileLayer, tileLayerOptions) {
+    function PokeMap(htmlElement, options) {
+
+        var coordinates = options.coordinates;
+        var zoomLevel = options.zoomLevel;
+        var timeRange = options.timeRange;
+        var apiEndpoint = options.apiEndpoint;
+        var tileLayer;
+        var tileLayerOptions;
+
+        if (!coordinates) {
+            coordinates = {
+                latitud: 0,
+                longitud: 0
+            }
+        }
+
+        if (!zoomLevel) {
+            zoomLevel = 3;
+        }
+
+        if (!timeRange) {
+            timeRange = 1;
+        }
+
+        if (!apiEndpoint) {
+            throw new Error('Fatal: apiEnpoint not defined');
+        }
 
         if (!tileLayer) {
             tileLayer = 'http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png';
             tileLayerOptions = {
                 attribution: '' +
-                             'JS16 <a href="https://github.com/PokemonGoers/PokeMap-1">PokeMap</a>, ' +
-                             'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> ' +
-                             'contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-                             'Imagery © <a href="http://thunderforest.com">Thunderforest/OpenCycleMap</a>, ' +
-                             'Pokemon Images © <a href="http://pokemondb.net/">Pokémon Database</a>',
-                maxZoom:     18
+                'JS16 <a href="https://github.com/PokemonGoers/PokeMap-1">PokeMap</a>, ' +
+                'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> ' +
+                'contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                'Imagery © <a href="http://thunderforest.com">Thunderforest/OpenCycleMap</a>, ' +
+                'Pokemon Images © <a href="http://pokemondb.net/">Pokémon Database</a>',
+                maxZoom: 18
             };
-        }
-
-        if (!coordinates) {
-            throw new Error('coordinates is not defined');
-        }
-
-        if (!zoomLevel) {
-            throw new Error('zoomLevel is not defined');
-        }
-
-        if (!timeRange) {
-            throw new Error('timeRange is not defined');
         }
 
         var self = this;
 
-        this.goto = goto;
+        this.goTo = goTo;
         this.updatePoint = updatePoints;
         this.on = on;
         self.timeRange = JSON.parse(JSON.stringify(timeRange));
@@ -44,7 +64,7 @@
         var eventHandlers = {};
         var mymap = null;
         var pokemonLayer = null;
-        var dataService = new DataService();
+        var dataService = new DataService(apiEndpoint);
 
         initMap();
 
@@ -52,8 +72,7 @@
 
             mymap = L.map(htmlElement);
             L.tileLayer(tileLayer, tileLayerOptions).addTo(mymap);
-            self.goto(coordinates, zoomLevel);
-
+            self.goTo({coordinates: coordinates, zoomLevel: zoomLevel});
             pokemonLayer = L.layerGroup([]).addTo(mymap);
 
             mymap.on('moveend', function (event) {
@@ -67,7 +86,7 @@
                 fireEvent('moveend', {
 
                     latlng: latlng,
-                    zoom:   zoom
+                    zoom: zoom
 
                 });
             });
@@ -135,9 +154,9 @@
                 to: mymap.getBounds().getSouthEast()
             };
 
-            dataService.getData(bounds, function(response) {
+            dataService.getData(bounds, function (response) {
 
-                if(response.data && response.data.length) {
+                if (response.data && response.data.length) {
 
                     pokemonLayer.clearLayers();
 
@@ -149,11 +168,15 @@
 
         }
 
-        function goto(coordinates, zoomLevel) {
+        function goTo(place) {
+            
+            var coordinate = place.coordinates;
+            var zoomLevel = place.zoomLevel;
+
             if (!zoomLevel) {
                 zoomLevel = mymap.getZoom();
             }
-            mymap.setView([coordinates.lat, coordinates.lng], zoomLevel);
+            mymap.setView([coordinates.latitud, coordinates.longitud], zoomLevel);
         }
 
         function updateTimeRange(timeRange) {
@@ -165,16 +188,16 @@
 
         var PokemonIcon = L.Icon.extend({
             options: {
-                iconSize:     [30, 30],
-                shadowSize:   [50, 64],
+                iconSize: [30, 30],
+                shadowSize: [50, 64],
                 shadowAnchor: [4, 62],
-                popupAnchor:  [-3, -76]
+                popupAnchor: [-3, -76]
             }
         });
 
         function addPokemonMarker(pokemon) {
 
-            var rootIconUrl = 'http://pokedata.c4e3f8c7.svc.dockerapp.io:65014/api/pokemon/id/' + pokemon.pokemonId + '/icon';
+            var rootIconUrl = dataService.getApiEndpointURL() + 'api/pokemon/id/' + pokemon.pokemonId + '/icon';
 
             var icon = new PokemonIcon({iconUrl: rootIconUrl});
             var coordinates = L.latLng(pokemon.location.coordinates[1], pokemon.location.coordinates[0]);
@@ -188,13 +211,12 @@
             function displayBasicPokeData(event) {
 
                 //TODO: check if the pokemonId of the clicked pokemon can be taken like that or not (probably not :))
-                getPokemonDetailsById(pokemon.pokemonId, function(response){
-
+                getPokemonDetailsById(pokemon.pokemonId, function (response) {
 
 
                 });
 
-             };
+            };
 
             return marker;
 
@@ -202,7 +224,7 @@
 
     }
 
-    function DataService() {
+    function DataService(apiEndpoint) {
 
         var self = this;
 
@@ -214,9 +236,9 @@
                 var locationTo = location.to.lng + ',' + location.to.lat;
 
                 var xhr = new XMLHttpRequest();
-                var url = 'http://pokedata.c4e3f8c7.svc.dockerapp.io:65014/api/pokemon/sighting/coordinates/from/' + locationFrom + '/to/' + locationTo;
+                var url = apiEndpoint + 'api/pokemon/sighting/coordinates/from/' + locationFrom + '/to/' + locationTo;
                 xhr.open("GET", url, true);
-                xhr.onreadystatechange = function() {
+                xhr.onreadystatechange = function () {
 
                     if (xhr.readyState === 4 && xhr.status === 200) {
 
@@ -241,9 +263,9 @@
                 var locationTo = location.to.lng + ',' + location.to.lat;
 
                 var xhr = new XMLHttpRequest();
-                var url = 'http://pokedata.c4e3f8c7.svc.dockerapp.io:65014/api/pokemon/sighting/coordinates/from/' + locationFrom + '/to/' + locationTo;
+                var url = apiEndpoint + 'api/pokemon/sighting/coordinates/from/' + locationFrom + '/to/' + locationTo;
                 xhr.open("GET", url, true);
-                xhr.onreadystatechange = function() {
+                xhr.onreadystatechange = function () {
 
                     if (xhr.readyState === 4 && xhr.status === 200) {
 
@@ -263,9 +285,9 @@
             getPokemonDetailsById: function (id, callback) {
 
                 var xhr = new XMLHttpRequest();
-                var url = 'http://pokedata.c4e3f8c7.svc.dockerapp.io:65014/api/pokemon/id/' + id;
+                var url = apiEndpoint + 'api/pokemon/id/' + id;
                 xhr.open("GET", url, true);
-                xhr.onreadystatechange = function() {
+                xhr.onreadystatechange = function () {
 
                     if (xhr.readyState === 4 && xhr.status === 200) {
 
@@ -282,7 +304,7 @@
 
             },
 
-            getPokemonDataByTimeRange: function(from, to, callback){
+            getPokemonDataByTimeRange: function (from, to, callback) {
 
                 //The way the URL is requested is a bit different from what Catch em All group was thinking. Maybe we
                 //need to talk to the Data team to change this API is requested (just in seconds or minutes before and after.
@@ -290,14 +312,14 @@
                 //TODO: To be rechecked. the range is not clear how should it be specified. Currently not working.
 
                 var currentTime = new Date();
-                var startTimeStamp = new Date(currentTime .getTime() + 1000*from);
+                var startTimeStamp = new Date(currentTime.getTime() + 1000 * from);
                 var startTimeStampString = startTimeStamp.toUTCString();
                 var range = to - from + 's';
 
                 var xhr = new XMLHttpRequest();
-                var url = 'http://pokedata.c4e3f8c7.svc.dockerapp.io:65014/api/pokemon/sighting/ts/' + startTimeStampString + '/range/' + range;
+                var url = apiEndpoint + 'api/pokemon/sighting/ts/' + startTimeStampString + '/range/' + range;
                 xhr.open("GET", url, true);
-                xhr.onreadystatechange = function() {
+                xhr.onreadystatechange = function () {
 
                     if (xhr.readyState === 4 && xhr.status === 200) {
 
@@ -320,6 +342,10 @@
 
             }
         }
+
+        self.getApiEndpointURL = function () {
+            return apiEndpoint;
+        };
 
         self.getData = function (bounds, updateCallback) {
 
@@ -350,86 +376,8 @@
                     pokemons.push(dbService.getPredictedData(bounds, updateCallback));
                     return pokemons;
 
-
                 }
             }
-
-            var mockPokemons = [
-                {
-                    id:          1,
-                    name:        'Rattata',
-                    coordinates: {
-                        lat: 48.262457,
-                        lng: 11.669183
-                    }
-                },
-                {
-                    id:          2,
-                    name:        'Pikachu',
-                    coordinates: {
-                        lat: 45.245842,
-                        lng: 14.674122
-                    }
-                },
-                {
-                    id:          3,
-                    name:        'Metapod',
-                    coordinates: {
-                        lat: 47.463472,
-                        lng: 12.169228
-                    }
-                },
-                {
-                    id:          4,
-                    name:        'Kakuna',
-                    coordinates: {
-                        lat: 46.162539,
-                        lng: 13.666696
-                    }
-                },
-                {
-                    id:          5,
-                    name:        'Arbok',
-                    coordinates: {
-                        lat: 44.467508,
-                        lng: 14.371981
-                    }
-                },
-                {
-                    id:          6,
-                    name:        'abra',
-                    coordinates: {
-                        lat: 48.1361,
-                        lng: 11.5810
-                    }
-                },
-                {
-                    id:          7,
-                    name:        'diglett',
-                    coordinates: {
-                        lat: 48.1471,
-                        lng: 11.5820
-                    }
-                },
-                {
-                    id:          8,
-                    name:        'clefairy',
-                    coordinates: {
-                        lat: 48.1441,
-                        lng: 11.5870
-                    }
-                },
-                {
-                    id:          9,
-                    name:        'dugtrio',
-                    coordinates: {
-                        lat: 48.1411,
-                        lng: 11.5715
-                    }
-                }
-            ];
-
-            return mockPokemons;
 
         };
 
@@ -437,7 +385,9 @@
 
     if (typeof define === 'function' && define.amd) {
         // AMD
-        define([], function () { return PokeMap; });
+        define([], function () {
+            return PokeMap;
+        });
     } else if (typeof module === 'object' && module.exports) {
         // CommonJS
         module.exports = PokeMap;
--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-var munichCoords = {
-    lat: 48.1351,
-    lng: 11.5820
-};
-
 var htmlElement = 'mapid'
+var options = {
+    // coordinates: {
+    //     latitud: 48.1351,
+    //     longitud: 11.5820
+    // },
+    // zoomLevel: 10,
+    // timeRange: 1,
+    apiEndpoint: 'http://pokedata.c4e3f8c7.svc.dockerapp.io:65014/'
+}
 
-var pokemap = new PokeMap(htmlElement, munichCoords, 10, 1);
+var pokemap = new PokeMap(htmlElement, options);
 
-pokemap.on('moveend', function(event) {
-
+pokemap.on('moveend', function (event) {
     // console.log(event.latlng.lat + ' ' + event.latlng.lng  + ' ' + event.zoom);
-
 });
