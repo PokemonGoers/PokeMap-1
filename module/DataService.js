f911930ea1ab98a8f8e18ec4114eb36d35526309
Gani Qinami <gani_qinami@hotmail.com>
2016-12-11 00:07:14 +0100

    fix the predictions

--- module/DataService.js
+++ module/DataService.js
@@ -60,13 +60,15 @@ function DataService(apiEndpoint) {
 
         },
 
-        getPredictedData: function (location, timestamp, callback) {
+        getPredictedData: function (coordinates, timestamp, callback) {
 
-            var locationFrom = location.from.lng + ',' + location.from.lat;
-            var locationTo = location.to.lng + ',' + location.to.lat;
+            var longitude = coordinates.longitude;
+            var latitude = coordinates.latitude;
+            var date = new Date();
+            var predictionsUntilDate = (new Date(new Date().getTime()-sightingsSince*1000))
 
             var xhr = new XMLHttpRequest();
-            var url = apiEndpoint + 'api/pokemon/prediction/coordinates/' + locationFrom + ',' + locationTo + '/ts/' + timestamp;
+            var url = apiEndpoint + 'api/pokemon/prediction/coordinates/' + longitude + ',' + latitude + '/ts/' + predictionsUntilDate.toUTCString();
             xhr.open("GET", url, true);
 
             xhr.onreadystatechange = function () {
@@ -145,14 +147,16 @@ function DataService(apiEndpoint) {
         return apiEndpoint;
     };
 
-    self.fetchData = function(sightingsSince, predictionsUntil, updateCallback) {
+    self.fetchData = function(sightingsSince, predictionsUntil, coordinates, updateCallback) {
+
+        debugger;
 
         if (sightingsSince == 0){
             if (predictionsUntil == 0){
                 console.log("Nothing to show");
                 return;
-            }
-            //code to get prediction data
+            } else
+            dbService.getPredictedData(coordinates, predictionsUntil, updateCallback);
         } else {
             if (predictionsUntil == 0){
                 //code to get sightings
@@ -161,6 +165,7 @@ function DataService(apiEndpoint) {
                 //code to get sightings
                 dbService.getPastDataByTime(sightingsSince, updateCallback);
                 //code to get predictions
+                dbService.getPredictedData(coordinates, predictionsUntil, updateCallback);
             }
 
 
--- script.js
+++ script.js
@@ -2,10 +2,10 @@ var htmlElement = 'mapid';
 var options = {
 
     filter : {
-        pokemonIds: null, sightingsSince: 1500000000, predictionsUntil: 0
+        pokemonIds: null, sightingsSince: 1500000000, predictionsUntil: 33333333
     },
-    apiEndpoint: 'http://pokedata.c4e3f8c7.svc.dockerapp.io:65014',
-    websocketEndpoint: 'pokedata.c4e3f8c7.svc.dockerapp.io:65024',
+    apiEndpoint: 'https://predictemall.online',
+    websocketEndpoint: 'https://predictemall.online',
     tileLayer: 'https://api.mapbox.com/styles/v1/poulzinho/ciu2fc21400k32iqi2gkb7h7g/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicG91bHppbmhvIiwiYSI6ImNpdTJmMmlwMTAwMHAyeW55NmVpbXpoY3oifQ._S-9Yx6OXlnMMq_MgsodlA',
     tileLayerOptions: {
         attribution: '' +
